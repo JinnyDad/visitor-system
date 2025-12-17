98965dec7013a1fae0a4b2949ce95daa6c521ab0
Stars <starspring08@naver.com>
2025-12-17 18:09:10 +0900

    force: kill all redirects and rename login folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
